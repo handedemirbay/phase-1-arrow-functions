b5fb89d141b65787809541a74c053be16625bc73
Hande Demirbay <handedemirbay@gmail.com>
2023-03-27 00:07:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(a, b){
+  if (b !== 0){
+    return a/b
+  }
+  return "Error, b can't be equal to 0!"
+};
+
+const square = (number) => number * number;
+
+const add = (number1, number2) => number1 + number2;
